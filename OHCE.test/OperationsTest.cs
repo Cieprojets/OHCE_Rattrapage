27af9bd21be127ee173168257ef995d631f2404e
Cieprojets <zineb.fahim@epsi.fr>
2023-05-04 13:19:07 +0200

    "Etape 1 : les bases , tous les operations avec les InlineData"

--- OHCE.test/OperationsTest.cs
+++ OHCE.test/OperationsTest.cs
@@ -17,6 +17,49 @@ namespace OHCE.test
             Assert.Equal(resAttendu, res);
            
         }
+
+
+        [Theory]
+        [InlineData(5,4,20)]
+        [InlineData(1,80,80)]
+        public void TestProduit(int x, int y, int resAttenduProduit)
+        {
+
+            Operation operation = new Operation();
+            int res = operation.Produit(x, y);
+
+            Assert.Equal(resAttenduProduit, res);
+
+        }
+
+        [Theory]
+        [InlineData(20, 5, 4,0)]
+        [InlineData(1, 1, 1, 0)]
+        public void TestDivision(int x, int y, int resAttenduQuotient ,int resAttenduReste)
+        {
+
+            Operation operation = new Operation();
+            (int quotient, int reste) res = operation.Division(x, y);
+
+            Assert.Equal(resAttenduQuotient, res.quotient);
+            Assert.Equal(resAttenduReste, res.reste);
+
+        }
+
+        [Theory]
+        [InlineData(7, 4, 3)]
+        [InlineData(2, 8, -6)]
+        public void TestDifference(int x, int y, int resAttenduDifference)
+        {
+
+            Operation operation = new Operation();
+            int res = operation.Difference(x, y);
+
+            Assert.Equal(resAttenduDifference, res);
+
+        }
+
+
     }
 
 }
--- OHCE/Operation.cs
+++ OHCE/Operation.cs
@@ -6,6 +6,24 @@ namespace OHCE
         {
           return x+y ;
         }
+
+        public int Produit(int x, int y)
+        {
+            return x * y;
+        }
+        public (int quotient, int reste) Division(int x, int y)
+        {
+           int quotient = x / y;
+            int reste = x % y;
+            return (quotient, reste);
+        }
+
+        public int Difference(int x, int y)
+        {
+           
+            return x - y;
+        }
+
     }
  
 }
