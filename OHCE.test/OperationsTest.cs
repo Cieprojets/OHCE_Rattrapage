e50fcb7a215e86e214fdaafeee84b698f37e89ee
Cieprojets <zineb.fahim@epsi.fr>
2023-05-04 17:09:37 +0200

    "Etape 2: decimaux , toutes les operations y figure "

--- OHCE.test/OperationsTest.cs
+++ OHCE.test/OperationsTest.cs
@@ -3,6 +3,8 @@ namespace OHCE.test
 
 {
     using Xunit;
+    using static OHCE.Operation;
+
     public class OperationsTest
     {
         [Theory]
@@ -58,6 +60,62 @@ namespace OHCE.test
             Assert.Equal(resAttenduDifference, res);
 
         }
+
+        [Theory]
+        [InlineData(2.3, 4.7,1, 7.0)]
+        [InlineData(2.56, 4.78,2, 7.34)]
+       public void TestAdditionPrecision(double x, double y,int precision, double resPrecisAttendu)
+        {
+
+            Operation operation = new Operation();
+            double res = operation.PrecisionSomme(x, y, precision);
+
+            Assert.Equal(resPrecisAttendu, res, precision);
+
+        }
+
+        [Theory]
+        [InlineData(4.3, 8.7, 2, 37.41)]
+        [InlineData(7.11, 5.555, 3, 39.496)]
+        public void TestProduitPrecision(double x, double y, int precision, double resPrecisProduitAttendu)
+        {
+
+            Operation operation = new Operation();
+            double res = operation.PrecisionProduit(x, y, precision);
+
+            Assert.Equal(resPrecisProduitAttendu, res);
+
+        }
+
+        [Theory]
+        [InlineData(12.5, 5.6, 3, 2.232)]
+        [InlineData(17.56, 89.999, 6, 0.195113)]
+
+        public void TestDivisionPrecision(double x, double y, int precision, double resPrecisDivisionAttendu)
+        {
+
+            var operation = new PrecisionDivision(precision);
+            double res = operation.Diviser(x, y);
+
+            Assert.Equal(resPrecisDivisionAttendu, res);
+
+        }
+
+        [Theory]
+        [InlineData(2.5, 1.3,1, 1.2)]
+        [InlineData(7.9, 4.562, 3, 3.338)]
+        public void TestDifferencePrecision(double x, double y, int precision, double resPrecisDifferenceAttendu)
+        {
+            var calculator = new PrecisionDifference(precision);
+
+            
+            var res = calculator.Difference(x, y);
+
+            
+            Assert.Equal(resPrecisDifferenceAttendu, res);
+        }
+
+
 
 
     }
--- OHCE/Operation.cs
+++ OHCE/Operation.cs
@@ -1,10 +1,10 @@
 namespace OHCE
 {
-    public  class Operation
+    public class Operation
     {
-        public  int Somme(int x, int y)
+        public int Somme(int x, int y)
         {
-          return x+y ;
+            return x + y;
         }
 
         public int Produit(int x, int y)
@@ -13,16 +13,63 @@ namespace OHCE
         }
         public (int quotient, int reste) Division(int x, int y)
         {
-           int quotient = x / y;
+            int quotient = x / y;
             int reste = x % y;
             return (quotient, reste);
         }
 
         public int Difference(int x, int y)
         {
-           
+
             return x - y;
         }
+
+        public double PrecisionSomme(double x, double y, int precision)
+        {
+            return Math.Round(x + y, precision);
+        }
+        public double PrecisionProduit(double x, double y, int precision)
+        {
+            return Math.Round(x * y, precision);
+        }
+
+        public class PrecisionDivision{
+            private int precision;
+            public PrecisionDivision(int precision)
+            {
+                this.precision = precision;
+            }
+            public double Diviser(double x, double y)
+            {
+                return Math.Round(x / y, precision);
+            }
+
+        }
+
+        public class PrecisionDifference
+        {
+            private int precision;
+            public PrecisionDifference(int precision)
+            {
+               this.precision = precision;
+            }
+            public double Difference(double x, double y)
+            {
+                return Math.Round(x - y, precision);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
  
