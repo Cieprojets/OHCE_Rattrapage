d767f441952127049d0f3ea00cd159fd1cd552d6
Cieprojets <zineb.fahim@epsi.fr>
2023-05-14 20:42:53 +0200

    etape1:les bases tests non reussis

--- OHCE.test/OperationsTest.cs
+++ OHCE.test/OperationsTest.cs
@@ -11,8 +11,8 @@ namespace OHCE.test
         //etape1:lesbases
 
         [Theory]
-        [InlineData(4,7,11)]
-        [InlineData(8, 8, 16)]
+        [InlineData(4,7,12)]
+        [InlineData(8, 8, 18)]
         public void TestAddition(int x, int y , int resAttendu)
         {
 
@@ -25,8 +25,8 @@ namespace OHCE.test
 
 
         [Theory]
-        [InlineData(5,4,20)]
-        [InlineData(1,80,80)]
+        [InlineData(5,4,25)]
+        [InlineData(1,80,87)]
         public void TestProduit(int x, int y, int resAttenduProduit)
         {
 
@@ -38,8 +38,8 @@ namespace OHCE.test
         }
 
         [Theory]
-        [InlineData(20, 5, 4,0)]
-        [InlineData(1, 1, 1, 0)]
+        [InlineData(20, 5, 4,1)]
+        [InlineData(1, 1, 1, 1)]
         public void TestDivision(int x, int y, int resAttenduQuotient ,int resAttenduReste)
         {
 
@@ -52,8 +52,8 @@ namespace OHCE.test
         }
 
         [Theory]
-        [InlineData(7, 4, 3)]
-        [InlineData(2, 8, -6)]
+        [InlineData(7, 4, 9)]
+        [InlineData(2, 8, -12)]
         public void TestDifference(int x, int y, int resAttenduDifference)
         {
 
@@ -65,75 +65,6 @@ namespace OHCE.test
         }
 
         
-        //etape2:lesdécimaux
-        [Theory]
-        [InlineData(2.3, 4.7,1, 7.0)]
-        [InlineData(2.56, 4.78,2, 7.34)]
-       public void TestAdditionPrecision(double x, double y,int precision, double resPrecisAttendu)
-        {
-
-            Operation operation = new Operation();
-            double res = operation.PrecisionSomme(x, y, precision);
-
-            Assert.Equal(resPrecisAttendu, res, precision);
-
-        }
-
-        [Theory]
-        [InlineData(4.3, 8.7, 2, 37.41)]
-        [InlineData(7.11, 5.555, 3, 39.496)]
-        public void TestProduitPrecision(double x, double y, int precision, double resPrecisProduitAttendu)
-        {
-
-            Operation operation = new Operation();
-            double res = operation.PrecisionProduit(x, y, precision);
-
-            Assert.Equal(resPrecisProduitAttendu, res);
-
-        }
-
-        [Theory]
-        [InlineData(12.5, 5.6, 3, 2.232)]
-        [InlineData(17.56, 89.999, 6, 0.195113)]
-
-        public void TestDivisionPrecision(double x, double y, int precision, double resPrecisDivisionAttendu)
-        {
-
-            var operation = new PrecisionDivision(precision);
-            double res = operation.Diviser(x, y);
-
-            Assert.Equal(resPrecisDivisionAttendu, res);
-
-        }
-
-        [Theory]
-        [InlineData(2.5, 1.3,1, 1.2)]
-        [InlineData(7.9, 4.562, 3, 3.338)]
-        public void TestDifferencePrecision(double x, double y, int precision, double resPrecisDifferenceAttendu)
-        {
-            var calculator = new PrecisionDifference(precision);
-
-            
-            var res = calculator.Difference(x, y);
-
-            
-            Assert.Equal(resPrecisDifferenceAttendu, res);
-        }
-
-
-        //etape3:tolérance
-
-        [Theory]
-        [InlineData("5\n + \n8", 13)]
-        [InlineData("7 + \n4", 11)]
-
-        public void TestSommeTolerance(string saisi, int resSommeToleranceAttendu)
-        {
-           
-            var operation = new Operation();
-            var res = operation.SommeTolerance(saisi);
-            Assert.Equal(resSommeToleranceAttendu, res);
-        }
 
     }
 
