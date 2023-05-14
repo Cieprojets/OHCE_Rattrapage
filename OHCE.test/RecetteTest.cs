3e53ac9ff0955f19a366abb8e16d5fee8d5c9888
Cieprojets <zineb.fahim@epsi.fr>
2023-05-14 23:26:46 +0200

    "etape4:fignolage , tests reussis"

--- OHCE.test/RecetteTest.cs
+++ OHCE.test/RecetteTest.cs
@@ -7,8 +7,8 @@ namespace OHCE.test
     public class RecetteTest
     {
         [Theory]
-        [InlineData("5.1415\n plus 2.7182", 4, "fr", 5.8597)]
-        [InlineData("78.1415 moins \n2.7182", 4, "fr", 0.4233)]
+        [InlineData("3.1415\n plus 2.7182", 4, "fr", 5.8597)]
+        [InlineData("3.1415 moins \n2.7182", 4, "fr", 0.4233)]
         public void Recette_1Test(string valeurs, int precision, string langue, double valeurAttendu)
         {
             var calculatrice = new CalculatricePrecision(precision, langue);
@@ -33,12 +33,12 @@ namespace OHCE.test
         private readonly Operation _operation = new Operation();
 
         [Theory]
-        [InlineData(5, 3, 5)]
-        [InlineData(2, 3, 23)]
-        [InlineData(0, 0, 4)]
-        [InlineData(0, 10, 12)]
-        [InlineData(-4, 5, -0)]
-        [InlineData(2, -7, 94)]
+        [InlineData(5, 3, 2)]
+        [InlineData(2, 3, 6)]
+        [InlineData(0, 0, 0)]
+        [InlineData(0, 10, 0)]
+        [InlineData(-4, 5, -20)]
+        [InlineData(2, -7, 9)]
         public void Recette_2Test(int x, int y, int valeurAttendu)
         {
             if (valeurAttendu == _operation.Produit(x, y))
@@ -60,11 +60,11 @@ namespace OHCE.test
         }
 
         [Theory]
-        [InlineData("2 plus\n 3", "en", 54)]
-        [InlineData("5 plus 6", "en", 116)]
-        [InlineData("0\n plus 10", "en", 102)]
-        [InlineData("6 dividedby 2", "en", 378)]
-        [InlineData("15 \ndivided by 5", "en", 352)]
+        [InlineData("2 plus\n 3", "en", 5)]
+        [InlineData("5 plus 6", "en", 11)]
+        [InlineData("0\n plus 10", "en", 10)]
+        [InlineData("6 dividedby 2", "en", 3)]
+        [InlineData("15 \ndivided by 5", "en", 3)]
         public void Recette_3Test(string valeurs, string langue, int valeurAttendu)
         {
             var calculatrice = new CalcultatriceMultiLangues();
