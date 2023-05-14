437b37326aa04d3254f8a07832de78c703248160
Cieprojets <zineb.fahim@epsi.fr>
2023-05-14 23:01:00 +0200

    "etape3:tolerance , tests reussis pour toutes les operations decim… …ales en mode enfant et langues francais et anglais , toutes les operations sont neutres aux espaces et aux sauts de ligne "

--- OHCE.test/CalculatricePrecisionTest.cs
+++ OHCE.test/CalculatricePrecisionTest.cs
@@ -5,9 +5,9 @@ namespace OHCE.test
     public class CalculatricePrecisionTest
     {
         [Theory]
-        [InlineData("2.3 plus 4.73", "fr", 2, 45.03)]
-        [InlineData("2.56 plus 4.78", "en", 23, 7.3)]
-        [InlineData("4.43 plus \n8.99", "en", 37, 3.420)]
+        [InlineData("2.3 plus 4.73", "fr", 2, 7.03)]
+        [InlineData("2.56 plus 4.78", "en", 1, 7.3)]
+        [InlineData("4.43 plus \n8.99", "en", 3, 13.420)]
         public void TestCalculerSommedecimaux(string valeurs, string langue, int precision, double expected)
         {
 
@@ -23,9 +23,9 @@ namespace OHCE.test
 
 
         [Theory]
-        [InlineData("2.3 fois 4.73", "fr", 2, 10.7)]
-        [InlineData("2.76 times 4.78", "en", 2, 12.24)]
-        [InlineData("4.73 times \n8.99", "en", 6, 39.83)]
+        [InlineData("2.3 fois 4.73", "fr", 2, 10.88)]
+        [InlineData("2.56 times 4.78", "en", 2, 12.24)]
+        [InlineData("4.43 times \n8.99", "en", 2, 39.83)]
         public void TestCalculerProduitdecimaux(string valeurs, string langue, int precision, double expected)
         {
 
@@ -41,9 +41,9 @@ namespace OHCE.test
 
 
         [Theory]
-        [InlineData("2.3 divisépar 4.73", "fr", 2, 0)]
-        [InlineData("2.56 dividedby 4.78", "en", 2, 0)]
-        [InlineData("4.43 divided by \n8.99", "en", 2, 0)]
+        [InlineData("2.3 divisépar 4.73", "fr", 2, 0.49)]
+        [InlineData("2.56 dividedby 4.78", "en", 2, 0.54)]
+        [InlineData("4.43 divided by \n8.99", "en", 2, 0.49)]
         public void TestCalculerDivisiondecimaux(string valeurs, string langue, int precision, double expected)
         {
 
@@ -57,9 +57,9 @@ namespace OHCE.test
         }
 
         [Theory]
-        [InlineData("27 moins 4.73", "fr", 1, -2.4)]
-        [InlineData("256 minus 4.78", "en", 2, -2.22)]
-        [InlineData("8999 minus \n 4.43", "en", 3, 4.569)]
+        [InlineData("2.3 moins 4.73", "fr", 1, -2.4)]
+        [InlineData("2.56 minus 4.78", "en", 2, -2.22)]
+        [InlineData("8.999 minus \n 4.43", "en", 3, 4.569)]
         public void TestCalculerDifferencedecimaux(string valeurs, string langue, int precision, double expected)
         {
 
